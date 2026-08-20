3a0d405476f4c8477e6b5dc4bd38618dd69d2454
Julian <163280302+JEstermann@users.noreply.github.com>
2026-08-20 21:44:59 +0200

    init backend

---
+++ backend/src/app.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.listen(5000, () => console.log("API lancée sur http://localhost:5000"));

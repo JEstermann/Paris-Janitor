4fad3b1fd914ecef078ebd72cff2da1621b3f712
Julian <163280302+JEstermann@users.noreply.github.com>
2026-09-07 22:35:59 +0200

    deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://paris-janitor-backend.onrender.com/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
